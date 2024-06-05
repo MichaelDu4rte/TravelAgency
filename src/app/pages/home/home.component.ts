89c3afa495a1f520822cf1b4b20c24e7ba4b38ae
MichaelDu4rte <johndoe@example.com>
2024-06-04 21:40:10 -0300

    footer  component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,11 +3,12 @@ import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { BannerComponent } from '../../components/banner/banner.component';
 import { PopularHousesComponent } from '../../components/popular-houses/popular-houses.component';
 import { ClientsTestimonyComponent } from '../../components/clients-testimony/clients-testimony.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent, BannerComponent, PopularHousesComponent, ClientsTestimonyComponent],
+  imports: [NavbarComponent, BannerComponent, PopularHousesComponent, ClientsTestimonyComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
