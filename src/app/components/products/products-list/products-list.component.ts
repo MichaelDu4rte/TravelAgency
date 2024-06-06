247ef8d64749acc9b138723c516853591c7784b7
MichaelDu4rte <johndoe@example.com>
2024-06-05 22:15:44 -0300

    add more products array

--- src/app/components/products/products-list/products-list.component.ts
+++ src/app/components/products/products-list/products-list.component.ts
@@ -12,12 +12,21 @@ export class ProductsListComponent implements OnChanges {
   @Input() criteria: string = '';
 
   products = [
-    { name: 'Plaza Hotel', price: 500, location: 'São Paulo, Brasil', imageUrl: 'https://images.pexels.com/photos/1396122/pexels-photo-1396122.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', popularity: 3 },
+    { name: 'Plaza Hotel', price: 500, location: 'São Paulo, Brasil', imageUrl: 'https://images.pexels.com/photos/1396122/pexels-photo-1396122.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', popularity: 5},
     { name: 'Panorama Duplex', price: 700, location: 'São Paulo, Brasil', imageUrl: 'https://images.pexels.com/photos/164558/pexels-photo-164558.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', popularity: 5 },
-    { name: 'Estadia Grandiosa', price: 800, location: 'São Paulo, Brasil', imageUrl: 'https://images.pexels.com/photos/206172/pexels-photo-206172.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', popularity: 4 },
-    { name: 'Cabana Vista Serena', price: 550, location: 'São Paulo, Brasil', imageUrl: 'https://images.pexels.com/photos/221540/pexels-photo-221540.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', popularity: 2 },
+    { name: 'Estadia Grandiosa', price: 800, location: 'São Paulo, Brasil', imageUrl: 'https://images.pexels.com/photos/206172/pexels-photo-206172.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', popularity: 5 },
+    { name: 'Cabana Vista Serena', price: 550, location: 'São Paulo, Brasil', imageUrl: 'https://images.pexels.com/photos/221540/pexels-photo-221540.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', popularity: 5 },
     { name: 'Paraíso Verde', price: 400, location: 'São Paulo, Brasil', imageUrl: 'https://images.pexels.com/photos/20507713/pexels-photo-20507713/free-photo-of-casas-residencias-jardim-casa.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', popularity: 1 },
-    { name: 'Villa Lumina', price: 300, location: 'São Paulo, Brasil', imageUrl: 'https://images.pexels.com/photos/14191118/pexels-photo-14191118.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', popularity: 3 }
+    { name: 'Villa Lumina', price: 300, location: 'São Paulo, Brasil', imageUrl: 'https://images.pexels.com/photos/14191118/pexels-photo-14191118.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', popularity: 5 },
+    { name: 'Residência Aconchegante', price: 600, location: 'São Paulo, Brasil', imageUrl: 'https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', popularity: 3 },
+    { name: 'Casa Bela Vista', price: 450, location: 'São Paulo, Brasil', imageUrl: 'https://images.pexels.com/photos/280229/pexels-photo-280229.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', popularity: 4 },
+    { name: 'Morada do Sol', price: 350, location: 'São Paulo, Brasil', imageUrl: 'https://images.pexels.com/photos/271624/pexels-photo-271624.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', popularity: 2 },
+    { name: 'Retiro Tranquilo', price: 480, location: 'São Paulo, Brasil', imageUrl: 'https://images.pexels.com/photos/323780/pexels-photo-323780.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', popularity: 3 },
+    { name: 'Casa do Lago', price: 650, location: 'São Paulo, Brasil', imageUrl: 'https://images.pexels.com/photos/276724/pexels-photo-276724.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', popularity: 4 },
+    { name: 'Apertamente Central', price: 550, location: 'São Paulo, Brasil', imageUrl: 'https://images.pexels.com/photos/594077/pexels-photo-594077.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', popularity: 3 },
+    { name: 'Chalé das Montanhas', price: 400, location: 'São Paulo, Brasil', imageUrl: 'https://images.pexels.com/photos/2091166/pexels-photo-2091166.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', popularity: 2 },
+    { name: 'Casa Campo Sereno', price: 500, location: 'São Paulo, Brasil', imageUrl: 'https://images.pexels.com/photos/237371/pexels-photo-237371.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', popularity: 4 },
+    { name: 'Cobertura Luxo', price: 750, location: 'São Paulo, Brasil', imageUrl: 'https://images.pexels.com/photos/276551/pexels-photo-276551.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', popularity: 3 }
   ];
 
   filteredProducts = [...this.products];
--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { ProductsListComponent } from '../../components/products/products-list/products-list.component';
 import { ProductsRecommendedComponent } from '../../components/products/products-recommended/products-recommended.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [NavbarComponent, ProductsListComponent, ProductsRecommendedComponent],
+  imports: [NavbarComponent, ProductsListComponent, ProductsRecommendedComponent, FooterComponent],
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss'
 })
