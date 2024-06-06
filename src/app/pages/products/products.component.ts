df573a201c6fddf3361b0a5c485df563747ea8a0
MichaelDu4rte <johndoe@example.com>
2024-06-05 21:27:32 -0300

    products-recommended component

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { ProductsListComponent } from '../../components/products/products-list/products-list.component';
+import { ProductsRecommendedComponent } from '../../components/products/products-recommended/products-recommended.component';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [NavbarComponent, ProductsListComponent],
+  imports: [NavbarComponent, ProductsListComponent, ProductsRecommendedComponent],
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss'
 })
